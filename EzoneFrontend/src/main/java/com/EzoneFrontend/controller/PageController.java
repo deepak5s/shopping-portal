4196785c11e2ce46eb3d1d366fac2ec0f2ae9cc8
deepak5s <import.deepak@protonmail.com>
2017-08-20 21:47:42 +0530

    ERROR FIXED IN FRONTEND

--- EzoneFrontend/src/main/java/com/EzoneFrontend/controller/PageController.java
+++ EzoneFrontend/src/main/java/com/EzoneFrontend/controller/PageController.java
@@ -1,5 +1,32 @@
 package com.EzoneFrontend.controller;
 
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
 public class PageController {
+	
+	@RequestMapping(value = {"/", "/home", "/index"})
+	public ModelAndView index(){
+		ModelAndView  mv= new ModelAndView ("page");
+		mv.addObject("greeting","spring Mvc");
+		return mv;
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 
 }
