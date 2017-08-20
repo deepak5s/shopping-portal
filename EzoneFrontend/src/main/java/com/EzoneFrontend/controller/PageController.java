3cad55138102b71f6c6b90938a51d48a08895167
deepak5s <import.deepak@protonmail.com>
2017-08-20 22:35:02 +0530

    Error Fixed

--- EzoneFrontend/src/main/java/com/EzoneFrontend/controller/PageController.java
+++ EzoneFrontend/src/main/java/com/EzoneFrontend/controller/PageController.java
@@ -16,17 +16,36 @@ public class PageController {
 		return mv;
 		
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+		@RequestMapping(value={"/about"})
 
-}
+		public ModelAndView about(){
+
+			ModelAndView mv=new ModelAndView("page");
+
+			mv.addObject("title", "Aboutus");
+
+			mv.addObject("userClickAbout", true);
+
+			return mv;
+
+			
+
+		}
+
+		@RequestMapping(value={"/contact"})
+
+		public ModelAndView contact(){
+
+			ModelAndView mv=new ModelAndView("page");
+
+			mv.addObject("title", "Contactus");
+
+			mv.addObject("userClickContact", true);
+
+			return mv;
+
+		}
+
+
+
+		}
